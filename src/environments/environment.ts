b0a898149ccfbf6045e2f57ae4ef51d80b0133e2
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-08-06 15:37:39 +0100

    firebase for auth added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,21 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const environment = {
-  production: false
+  apiKey: "AIzaSyAgcEkAJRVkETAInpSnppEuoHxzW9UEUVQ",
+  authDomain: "pet-care-ebec9.firebaseapp.com",
+  projectId: "pet-care-ebec9",
+  storageBucket: "pet-care-ebec9.firebasestorage.app",
+  messagingSenderId: "35911317743",
+  appId: "1:35911317743:web:55d62b1b3f5f7e8caa431a",
+  measurementId: "G-QW2CP1DGFB"
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Initialize Firebase
+const app = initializeApp(environment);
+const analytics = getAnalytics(app);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth'; // if using auth
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; // if using firestore
+import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideFirebaseApp(() => initializeApp(environment)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
