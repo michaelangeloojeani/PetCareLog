4b95668d0b21c002d60b91c2f1c581fcfd70711c
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-08-13 00:25:31 +0100

    added navigation set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./home/home.page').then(m => m.HomePage) },
+  { path: 'login', loadComponent: () => import('./login/login.page').then(m => m.LoginPage) },
+  { path: 'register', loadComponent: () => import('./register/register.page').then(m => m.RegisterPage) },
 ];
