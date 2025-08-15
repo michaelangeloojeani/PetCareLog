b3ffba405ed8221bed177fbb52790d6d03349f7b
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-08-15 21:24:41 +0100

    adjusted login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule, FormsModule, RouterLink],
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router, private toast: ToastController) {}
 
-  ngOnInit() {
+  async login() {
+    try {
+      await this.auth.login(this.email, this.password);
+      this.router.navigateByUrl('/home');
+    } catch (e: any) {
+      const t = await this.toast.create({ message: e.message || 'Login failed', duration: 1500 });
+      await t.present();
+    }
   }
-
 }
