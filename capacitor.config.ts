384ee1da986c77a145636944690d14e8555553f7
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-08-29 14:56:50 +0100

    last attempts to fix issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.petcare',
   appName: 'pet-care-log',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com", "facebook.com", "twitter.com"]
+    }
+  }
 };
-
 export default config;
+
